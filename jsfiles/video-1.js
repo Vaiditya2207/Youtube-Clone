d1e24e8f450306f3682103f5cdddb242401071f9
Vaiditya2207 <vaidityatanwar2207@gmail.com>
2023-12-31 22:59:16 +0530

    page-1

---
+++ jsfiles/video-1.js
@@ -0,0 +1,15 @@
+function signin() {
+    window.location.href = "signin.html"
+}
+
+function subscribe() {
+    let is_sub = document.getElementById("subscribe").innerHTML;
+    if (is_sub === "Subscribe") {
+        document.getElementById("subscribe").innerHTML = "Subscribed";
+        document.getElementById("subscribe").classList.add("subscribed");
+    } else {
+        document.getElementById("subscribe").innerHTML = "Subscribe";
+        document.getElementById("subscribe").classList.remove("subscribed");
+    }
+}
+
